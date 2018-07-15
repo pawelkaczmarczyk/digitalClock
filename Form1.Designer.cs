fcdeaf7e0fc57980698e952e9adc02b9728f00be
Paweł <pawel.k90@live.com>
2018-07-15 12:26:58 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace digitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hourLabel = new System.Windows.Forms.Label();
+            this.secondLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.dayLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // hourLabel
+            // 
+            this.hourLabel.Font = new System.Drawing.Font("Consolas", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.hourLabel.ForeColor = System.Drawing.Color.White;
+            this.hourLabel.Location = new System.Drawing.Point(12, 5);
+            this.hourLabel.Name = "hourLabel";
+            this.hourLabel.Size = new System.Drawing.Size(184, 70);
+            this.hourLabel.TabIndex = 0;
+            this.hourLabel.Text = "00:00";
+            // 
+            // secondLabel
+            // 
+            this.secondLabel.Font = new System.Drawing.Font("Consolas", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.secondLabel.ForeColor = System.Drawing.Color.White;
+            this.secondLabel.Location = new System.Drawing.Point(202, 5);
+            this.secondLabel.Name = "secondLabel";
+            this.secondLabel.Size = new System.Drawing.Size(89, 70);
+            this.secondLabel.TabIndex = 1;
+            this.secondLabel.Text = "00";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.Font = new System.Drawing.Font("Consolas", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateLabel.ForeColor = System.Drawing.Color.White;
+            this.dateLabel.Location = new System.Drawing.Point(12, 75);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(342, 70);
+            this.dateLabel.TabIndex = 2;
+            this.dateLabel.Text = "01.01.2018";
+            // 
+            // dayLabel
+            // 
+            this.dayLabel.Font = new System.Drawing.Font("Consolas", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dayLabel.ForeColor = System.Drawing.Color.White;
+            this.dayLabel.Location = new System.Drawing.Point(12, 145);
+            this.dayLabel.Name = "dayLabel";
+            this.dayLabel.Size = new System.Drawing.Size(391, 70);
+            this.dayLabel.TabIndex = 3;
+            this.dayLabel.Text = "Poniedziałek";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(424, 223);
+            this.Controls.Add(this.dayLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.secondLabel);
+            this.Controls.Add(this.hourLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DigitalClock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hourLabel;
+        private System.Windows.Forms.Label secondLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label dayLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace digitalClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            hourLabel.Text = DateTime.Now.ToString("HH:mm");
+            secondLabel.Text = DateTime.Now.ToString("ss");
+            dateLabel.Text = DateTime.Now.ToString("dd.MM.yyyy");
+            dayLabel.Text = DateTime.Now.ToString("dddd");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
